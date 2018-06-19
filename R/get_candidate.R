edbd105597ae5c61792ed1cf6e40d750d95eed60
DavytJ <joselinadavyt@gmail.com>
2018-06-19 13:13:36 -0300

    change in get_candidate

--- R/get_candidate.R
+++ R/get_candidate.R
@@ -1,21 +1,22 @@
 #' Get Candidate Info
 #' GET https://api.propublica.org/campaign-finance/v1/{cycle}/candidates/{fec-id}
 #' @param FEC_ID The FEC-assigned 9-character ID of a candidate.
-#' @param cycle The election cycle
+#' @param cycle The election cycle must be even-numbered year between 1996 and 2018
 #'
-#' @return
+#' @return a list object with the return values about candidate basic information
 #' @export
 #'
 #' @examples
-#' \dontrun{
 #' get_candidate('P60005915', 2016)
 #' }
 get_candidate <- function(FEC_ID, cycle=2018, myAPI_Key){
   API = 'campaign-finance'
-  capture.output(tryCatch({
-    if(is.character(FEC_ID)){
-    query <- sprintf("%s/candidates/%s.json", cycle,FEC_ID)
-    pp_query(query, API, myAPI_Key = myAPI_Key)} else ('FEC_ID has to be character')
-    }))
+  if(!validate_cycle(cycle)){
+    stop('Incorrect cycle')
+  }
+  if(!is.character(FEC_ID)){
+    stop('FEC_ID has to be character')} else {
+    query <- sprintf("%s/candidates/%s.json", cycle, FEC_ID)
+    pp_query(query, API, myAPI_Key = myAPI_Key)}  
 }
 
