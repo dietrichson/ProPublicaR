2a4799e94ed7742b42c01c9c50b0403d487f707c
DavytJ <joselinadavyt@gmail.com>
2018-06-29 17:24:54 -0300

    get_recently_candidates done, issue fix in ProPublica API

---
+++ R/get_recently_candidates.R
@@ -0,0 +1,24 @@
+#' Get Top 20 Candidates in Specific Financial Category
+#' 
+#' HTTP Request
+#' GET https://api.propublica.org/campaign-finance/v1/{cycle}/candidates/new
+#'
+#' @param cycle The election cycle: Four-digit even-numbered year between 2010 and 2018
+#' @param myAPI_Key  use the Campaign Finance API, you must sign up for an API key. The API key must be included in all API requests to the server, set as a header.
+#'
+#' @return List of returned JSON from endpoint that retrieves ithe 20 most recently added FEC candidates for a given campaign cycle from the campaign-finance API from ProPublica. 
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_recently_candidates(2016)
+#' }
+get_recently_candidates <- function(cycle=2018, myAPI_Key){
+  API = 'campaign-finance'
+  if(!validate_cycle(cycle))
+    stop("Incorrect cycle")
+  if(cycle >= 2010 & cycle %% 2 == 0){
+    query <- sprintf("%s/candidates/new.json", cycle)
+    pp_query(query, API, myAPI_Key = myAPI_Key)
+  } else {stop("Cycle should be four-digit even-numbered year between 2010 and 2018")}
+}
---
+++ tests/testthat/test-get_recently_candidates.R
@@ -0,0 +1,15 @@
+context("get_recently_candidates")
+
+test_that("expected errors", {
+  expect_warning(expect_error(get_recently_candidates(16), "Incorrect cycle"), "Cycle should be four-digit year")
+  expect_error(get_recently_candidates(1996), "Cycle should be four-digit even-numbered year between 2010 and 2018")
+  expect_error(get_recently_candidates(2008), "Cycle should be four-digit even-numbered year between 2010 and 2018")
+  expect_warning(expect_error(get_recently_candidates(1995), "Incorrect cycle"), "Cycle should be four-digit year largest than 1996")
+  expect_warning(expect_error(get_recently_candidates(2007), "Incorrect cycle"), "Cycle should be even-numbered year largest than 1996")
+})
+
+test_that("expected lengths", {
+  expect_length(get_recently_candidates(2016), 5)
+  
+})
+
