dec488502f045a38d32227926e9780cfcc4f1786
Aleksander Dietrichson <dietrichson@gmail.com>
2020-04-11 15:59:07 -0300

    Added news.md Bumped version number Adjusted tests

---
+++ news.md
@@ -0,0 +1,21 @@
+## Version 1.1.1
+
+April 11 2020
+
+Updates:
+Some bug-fixes
+Unified approach to pagination
+
+## Version 1.1.0
+
+April 8 2020
+
+Updates:
+Included access to 116th congress 
+Added support for pagination
+Added support for key as environment variable
+
+## Version 0.9.1
+
+Initial Release on CRAN
+
--- tests/testthat/test-pp_query.R
+++ tests/testthat/test-pp_query.R
@@ -1,13 +1,5 @@
 context("pp_query")
 
-test_that("get error if API key is null", {
-  
-  expect_error(pp_query('115/bills/hr21.json',API = 'congress', version='v1', myAPI_Key = NULL))
-  
-  expect_error(pp_query('2018/races/MI/house/11.json', API='campaign-finance',version='v1', myAPI_Key = NULL))
-  
-})
-
 test_that("get error if API not support", {
   expect_error(pp_query('2018/races/MI/house/11.json', API='myAPI',version='v1', myAPI_Key = "PLgSaGES7ne8SLcWnwxAL2DeddgwuSLK4lQD34vj"), "API not supported.")
 })
