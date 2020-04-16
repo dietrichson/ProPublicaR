701a5803dcacc81bf446947c5da3fb3c02227c5d
Aleksander Dietrichson <dietrichson@gmail.com>
2020-04-16 13:14:25 -0300

    Removed unused code

--- tests/testthat/test-get_top20_candidate_of_FinancialCategory.R
+++ tests/testthat/test-get_top20_candidate_of_FinancialCategory.R
@@ -9,13 +9,6 @@ test_that("expected lengths", {
   categories <- c('candidate-loan', 'contribution-total', 'debts-owed', 'disbursements-total', 'end-cash', 'individual-total', 
                   'pac-total', 'receipts-total', 'refund-total')
   expect_length(get_top20_candidate_of_FinancialCategory(categories[1], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[2], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[3], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[4], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[5], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[6], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[7], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[8], 1996), 6)
-  # expect_length(get_top20_candidate_of_FinancialCategory(categories[9], 1996), 6)
+ 
 })
 
